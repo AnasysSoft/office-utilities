ad38d5d2b6ee34e6d53ec78ebb850cde7c970680
Farhad S <fd.shakouri@gmail.com>
2025-10-21 23:21:55 +0330

    initial client app

---
+++ client/src/app/modules/meal-reservation/meal-reservation-dashboard/meal-reservation-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-reservation-dashboard',
+  imports: [],
+  templateUrl: './meal-reservation-dashboard.html',
+  styleUrl: './meal-reservation-dashboard.scss'
+})
+export class MealReservationDashboard {
+
+}
---
+++ client/src/app/modules/meal-reservation/meal-selection/meal-selection.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-selection',
+  imports: [],
+  templateUrl: './meal-selection.html',
+  styleUrl: './meal-selection.scss'
+})
+export class MealSelection {
+
+}
