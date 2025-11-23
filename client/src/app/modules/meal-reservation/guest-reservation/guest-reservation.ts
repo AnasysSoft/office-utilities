cc13b9eb860019f6897d67885853774d8c2d05f9
Farhad Shakouri <78930102+Farhad1983@users.noreply.github.com>
2025-11-23 16:44:33 +0330

    guest reserve

--- client/src/app/modules/meal-reservation/guest-reservation/guest-reservation.ts
+++ client/src/app/modules/meal-reservation/guest-reservation/guest-reservation.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { baseForm } from '../../infrastructure/base-form';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { Checkbox } from 'primeng/checkbox';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { IconField } from 'primeng/iconfield';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InputIcon } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { PtIcon } from '../../../shared/pt-icon/pt-icon';
 
 @Component({
   selector: 'app-guest-reservation',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, PtIcon, ButtonModule, FloatLabelModule, Checkbox, IftaLabelModule, InputTextModule, IconField, InputIcon],
   templateUrl: './guest-reservation.html',
   styleUrl: './guest-reservation.scss'
 })
-export class GuestReservation {
+export class GuestReservation extends baseForm {
 
+  override initialForm(): void {
+    this.entityForm = this._formBuilder.group({
+			fullName: [null, [Validators.required]],
+			date:[null, [Validators.required]],
+			selectedMealIds:[null]
+		});
+
+    this.validationMessages = {
+			fullName: {
+				required: 'وارد کردن نام میهمان اجباری است', 
+			},
+			date: {
+				required: 'وارد کردن تاریخ رزرو اجباری است', 
+			}
+		};
+
+  }
+
+  guestRezerve() {
+    
+  }
+  
 }
